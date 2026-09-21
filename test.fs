f2a5f07005ace38bc26967e774c1ebb7de07b1e0
pavellitvinov <lit.pk.05@gmail.com>
2026-09-21 20:06:10 +0300

    initial commit

---
+++ test.fs
@@ -0,0 +1,14 @@
+// A line comment.
+/// XML documentation comment.
+
+(* A block comment. *)
+
+(*
+    A multiline block comment.
+    (* A nested block comment. *)
+    The string "*)" does not close the comment.
+    The verbatim string @"*)" does not close it either.
+    The triple string """*)""" does not close it.
+*)
+
+let value = 42 // A trailing comment.
